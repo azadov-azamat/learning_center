f8a4fdcdf798b354ad3f842c4eca0bf6b04f6daa
azamat_azadov <azamatazadov112@gmail.com>
2022-04-02 03:37:25 +0500

    clear rubbish components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import {Route, Switch} from "react-router-dom";
 import HomePage from "./component/pages/HomePage";
-import Register from "./component/pages/Register";
-import LoginJs from "./component/pages/LoginJs";
-import ActiveCourses from "./component/pages/ActiveCourses";
-import AdminPage from "./component/pages/AdminPage";
-import MentorPage from "./component/pages/MentorPage";
-import OperatorPage from "./component/pages/OperatorPage";
-import NotFound from "./component/pages/NotFound";
 
 function App() {
     return (
         <Switch>
             <Route exact path='/' component={HomePage}/>
-            <Route path='/signIn' component={LoginJs}/>
-            <Route path='/signUp' component={Register}/>
-            <Route path='/activeCourses' component={ActiveCourses}/>
-            <Route path='/adminPage' component={AdminPage}/>
-            <Route path='/mentorPage' component={MentorPage}/>
-            <Route path='/operatorPage' component={OperatorPage}/>
-            <Route component={NotFound}/>
         </Switch>
     );
 }
